8bda4fa57a12f0fe18e8cb8b4f2c5be291255608
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-08-30 21:06:36 +0300

    Improve the my vote page
    - Redirect to login page if not logged in
    - Ensure all orders are checked out in order to show the page

--- app/controllers/concerns/decidim/budgeting_pipeline/orders_controller_extensions.rb
+++ app/controllers/concerns/decidim/budgeting_pipeline/orders_controller_extensions.rb
@@ -10,13 +10,14 @@ module Decidim
       include Decidim::BudgetingPipeline::ProjectItemUtilities # Overrides the `voted_for?` method for multiple orders.
 
       included do
-        before_action :ensure_orders!, only: [:index]
+        before_action :ensure_checked_out_orders!, only: [:index]
       end
 
       private
 
-      def ensure_orders!
-        return if current_user && current_orders.any?
+      def ensure_checked_out_orders!
+        return redirect_to decidim.new_user_session_path unless user_signed_in?
+        return if current_orders.any? && current_orders.all?(&:checked_out?)
 
         flash[:warning] = I18n.t("decidim.budgets.orders.index.not_voted")
         redirect_to projects_path
