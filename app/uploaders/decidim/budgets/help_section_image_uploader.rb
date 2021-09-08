bbc80fe5c7a18ae5242689fe75316a9a49db0c54
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-09-08 10:09:25 +0300

    Change help section image sizes

--- app/uploaders/decidim/budgets/help_section_image_uploader.rb
+++ app/uploaders/decidim/budgets/help_section_image_uploader.rb
@@ -5,10 +5,10 @@ module Decidim
     # This class deals with uploading help section images in participatory
     # budgeting.
     class HelpSectionImageUploader < Decidim::RecordImageUploader
-      process resize_to_limit: [2080, 800]
+      process resize_to_limit: [2000, 830]
 
       version :small do
-        process resize_to_fill: [1160, 800]
+        process resize_to_fill: [1100, 460]
       end
     end
   end
