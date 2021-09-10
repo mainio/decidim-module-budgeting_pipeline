4788f40ebed74934eb4b68315db9d2e19f518627
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-09-10 19:20:29 +0300

    Fix issues for fetching the default language content for alternative langs
    When the geocoded data was fetched for a non-default language,
    there could be empty strings returned unless the default language
    content is used as a fallback.

--- app/model/concerns/decidim/budgeting_pipeline/project_extensions.rb
+++ app/model/concerns/decidim/budgeting_pipeline/project_extensions.rb
@@ -11,8 +11,6 @@ module Decidim
 
       class_methods do
         def geocoded_data_for(component)
-          locale = Arel::Nodes.build_quoted(I18n.locale.to_s).to_sql
-
           default_latitude = 0.0
           default_longitude = 0.0
 
@@ -25,15 +23,15 @@ module Decidim
 
           joins(:budget).pluck(
             :id,
-            "decidim_budgets_projects.title->>#{locale}",
-            "decidim_budgets_projects.summary->>#{locale}",
-            "decidim_budgets_projects.description->>#{locale}",
+            "CASE #{locale_case("decidim_budgets_projects.title")} END AS geotitle",
+            "CASE #{locale_case("decidim_budgets_projects.summary")} END AS geosummary",
+            "CASE #{locale_case("decidim_budgets_projects.description")} END AS geodescription",
             Arel.sql(
               <<~SQLCASE
                 CASE
                   WHEN CHAR_LENGTH(decidim_budgets_projects.address::text) > 0 THEN decidim_budgets_projects.address
-                  ELSE decidim_budgets_budgets.title->>#{locale}
-                END
+                  #{locale_case("decidim_budgets_budgets.title")}
+                END AS geoaddress
               SQLCASE
             ),
             Arel.sql(
@@ -42,7 +40,7 @@ module Decidim
                   WHEN decidim_budgets_projects.latitude IS NOT NULL THEN decidim_budgets_projects.latitude
                   WHEN decidim_budgets_budgets.center_latitude IS NOT NULL THEN decidim_budgets_budgets.center_latitude
                   ELSE #{default_latitude}
-                END
+                END AS geolatitude
               SQLCASE
             ),
             Arel.sql(
@@ -51,10 +49,24 @@ module Decidim
                   WHEN decidim_budgets_projects.longitude IS NOT NULL THEN decidim_budgets_projects.longitude
                   WHEN decidim_budgets_budgets.center_longitude IS NOT NULL THEN decidim_budgets_budgets.center_longitude
                   ELSE #{default_longitude}
-                END
+                END AS geolongitude
               SQLCASE
             )
           )
+        end
+
+        private
+
+        def locale_case(column)
+          locale = Arel::Nodes.build_quoted(I18n.locale.to_s).to_sql
+          default_locale = Arel::Nodes.build_quoted(I18n.default_locale.to_s).to_sql
+
+          return "WHEN true THEN #{column}->>#{locale}" if locale == default_locale
+
+          <<~SQLCASE
+            WHEN CHAR_LENGTH((#{column}->>#{locale})::text) > 0 THEN #{column}->>#{locale}
+            ELSE #{column}->>#{default_locale}
+          SQLCASE
         end
       end
 
