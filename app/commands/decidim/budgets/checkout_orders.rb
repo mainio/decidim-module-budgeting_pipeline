ed59aeb3152c377a7c18bad1f90b100d112bddd6
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-10-19 22:07:54 +0300

    Prevent duplicate empty votes in edge case scenario
    When the vote is cast exactly at the same time on two separate
    workers/servers, there can be an extra "empty" vote recorded for
    the user. The last vote is always the "truth" as the previous vote
    will not have any orders after they are mapped with the latest
    vote.

--- app/commands/decidim/budgets/checkout_orders.rb
+++ app/commands/decidim/budgets/checkout_orders.rb
@@ -22,6 +22,13 @@ module Decidim
         return broadcast(:invalid, orders) if orders.empty?
         return broadcast(:invalid, orders) if orders.any?(&:invalid?)
 
+        # This ensures that if the user happens to submit their vote twice in a
+        # row, the vote record won't be duplicated. It would not affect the
+        # amount of votes for projects as the other duplicate vote gets "cleared
+        # out" when the orders are mapped to the new vote but it would affect
+        # the reporting of the ongoing voting.
+        return broadcast(:ok, orders) if existing_vote.present?
+
         checkout_orders
         create_vote
         send_summary
@@ -52,6 +59,10 @@ module Decidim
           },
           visibility: "private-only"
         )
+      end
+
+      def existing_vote
+        @existing_vote ||= Decidim::Budgets::Vote.where(component: orders.first.component, user: user)
       end
 
       def send_summary
