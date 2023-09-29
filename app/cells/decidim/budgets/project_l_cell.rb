6f6686dd6d307fa88acc6be313879910d5cad427
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-09-29 17:08:20 +0300

    Migrate to the new layout

---
+++ app/cells/decidim/budgets/project_l_cell.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "cell/partial"
+
+module Decidim
+  module Budgets
+    # This cell renders a project with its L-size card.
+    class ProjectLCell < Decidim::Budgets::ProjectMCell
+      def card_classes
+        classes = super
+        classes = classes.split unless classes.is_a?(Array)
+        (classes + ["card--full"]).join(" ")
+      end
+
+      private
+
+      def resource_image_variant
+        :thumbnail_box
+      end
+
+      def category_image_variant
+        :card_box
+      end
+    end
+  end
+end
