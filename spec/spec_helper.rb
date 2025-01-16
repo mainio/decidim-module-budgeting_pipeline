494400de539643452deaabc1f99b81ea3cf46c2a
Joonas <joonas.aapro@mainiotech.fi>
2025-01-16 13:55:05 +0200

    Reverse workflow / Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,8 @@ ENV["ENGINE_ROOT"] = File.dirname(__dir__)
 
 # Ensure we can run the concurrency tests without problems
 ENV["RAILS_MAX_THREADS"] = "30"
+ENV["NODE_ENV"] ||= "test"
 
-Decidim::Dev.dummy_app_path =
-  File.expand_path(File.join(__dir__, "decidim_dummy_app"))
+Decidim::Dev.dummy_app_path = File.expand_path(File.join("spec", "decidim_dummy_app"))
 
 require "decidim/dev/test/base_spec_helper"
